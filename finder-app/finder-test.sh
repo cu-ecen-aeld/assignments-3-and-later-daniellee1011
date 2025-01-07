679bba5e693af604f43a301d5908ec34d0bd2f76
daniellee1011 <kauvoi1011@gmail.com>
2025-01-06 22:45:23 -0500

    [a4 part2] Update finder-test.sh
    - In the Buildroot environment, configuration files should be placed in the /etc directory on the target root filesystem
    - In the Buildroot environment, all executables (like writer) will be placed in the /usr/bin directory, which is included in the system's PATH. Therefore, you don't need to specify the relative path (./) to the writer program. Instead, you can directly call writer from anywhere because it's in the PATH.
    - As part of the Assignment 4 Part 2 requirements, the finder output must be written to a file called /tmp/assignment4-result.txt on the QEMU virtual machine.

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -41,7 +41,7 @@ rm -rf "${WRITEDIR}"
 mkdir -p "$WRITEDIR"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -60,10 +60,11 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+echo ${OUTPUTSTRING} > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
